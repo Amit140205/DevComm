29acf9a69ee57de3bbb54706cae55db725a9f36a
Amit140205 <rumaamitmondal340@gmail.com>
2025-07-04 18:28:07 +0530

    fixing ui

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,18 +1,21 @@
+
 import React from 'react'
 import SideBar from './Sidebar.jsx'
 import Navbar from './Navbar.jsx'
 
 const Layout = ({children, showSideBar=false}) => {
   return (
-    <div className='min-h-screen'>
-        <div className='flex'>
+    <div className='min-h-screen bg-base-100'>
+        <div className='flex min-h-screen'>
             {showSideBar && <SideBar/>}
 
             <div className='flex-1 flex flex-col'>
               <Navbar/>
 
-              <main className='flex-1 overflow-y-auto'>
-                {children}
+              <main className='flex-1 overflow-y-auto bg-base-100'>
+                <div className='min-h-full'>
+                  {children}
+                </div>
               </main>
             </div>
         </div>
